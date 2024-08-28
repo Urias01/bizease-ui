df594e5e2f6f0b963bd68ff076186c78c5906caa
Urias Pereira <uriasluis64@gmail.com>
2024-08-27 22:00:39 -0300

    feat: Add color palette and change theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from "./components/theme/theme-provider";
+import { ThemeToggle } from "./components/theme/theme-toggle";
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <>
-      <h1>BizEase</h1>
-    </>
-  )
+    <ThemeProvider defaultTheme="system" storageKey="bizease-theme">
+      <Button asChild>
+        <ThemeToggle></ThemeToggle>
+      </Button>
+      <h1>Biz Ease</h1>
+    </ThemeProvider>
+  );
 }
