0b9f369b0f81545d2e0b7c4a43bbdd1272c460d9
Urias <uriasluis64@gmail.com>
2024-09-04 13:27:12 -0300

    feat: adds a bit of responsiveness

--- src/pages/_layouts/_app-layout.tsx
+++ src/pages/_layouts/_app-layout.tsx
@@ -4,10 +4,10 @@ import { Box, Home, PackageOpen } from "lucide-react";
 
 export function AppLayout() {
   return (
-    <section className="flex">
-      <div className="flex-none h-screen bg-secondary items-center">
+    <section className="flex h-min-screen">
+      <div className="flex-none bg-secondary items-center">
         <h1 className="flex align-middle ml-2 mt-4 text-xl">
-          <Box className="h-6 w-6 mt-1 mr-2"/>
+          <Box className="h-6 w-6 mt-1 mr-2" />
           BizEase
         </h1>
         <div className="flex flex-col space-y-4 mt-8 w-36">
@@ -26,7 +26,9 @@ export function AppLayout() {
         <main className="flex flex-1 flex-col gap-4 p-8 pt-6">
           <Outlet />
         </main>
-        <p className="text-[0.75rem] absolute bottom-0 p-4">&copy; - BizEase {new Date().getFullYear()}</p>
+        <p className="text-[0.75rem] absolute bottom-0 p-4">
+          &copy; - BizEase {new Date().getFullYear()}
+        </p>
       </div>
     </section>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ export function Home() {
   return (
     <>
       <h1>BizEase Home</h1>
-      <section className="grid grid-cols-4 gap-4">
+      <section className="grid grid-cols-1 md:grid-cols-4 gap-4">
         <div className="border rounded-md p-8">
           <h2 className="text-xl font-bold">Título</h2>
           <p>Description</p>
@@ -36,8 +36,8 @@ export function Home() {
           </p>
         </div>
       </section>
-      <section className="flex gap-4 h-96">
-        <div className="flex flex-col gap-4 border rounded-md p-8 w-3/4">
+      <section className="flex flex-col md:flex-row gap-4 h-96">
+        <div className="flex flex-col gap-4 border rounded-md p-8 w-full md:w-3/4 overflow-hidden">
           <h2 className="text-xl font-bold">Título</h2>
           <p>Gráfico</p>
           <p>
@@ -46,22 +46,22 @@ export function Home() {
             cupiditate, magni amet autem reprehenderit laudantium quaerat labore
             quis deserunt nulla reiciendis fugit impedit itaque maxime quidem.
             Odit ducimus possimus nam, cum non facilis laudantium dolorum quod
-            harum deserunt, atque ad explicabo vero aliquid?
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Voluptatibus voluptate ullam veniam omnis quibusdam totam alias
-            cupiditate, magni amet autem reprehenderit laudantium quaerat labore
-            quis deserunt nulla reiciendis fugit impedit itaque maxime quidem.
-            Odit ducimus possimus nam, cum non facilis laudantium dolorum quod
-            harum deserunt, atque ad explicabo vero aliquid?
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Voluptatibus voluptate ullam veniam omnis quibusdam totam alias
-            cupiditate, magni amet autem reprehenderit laudantium quaerat labore
-            quis deserunt nulla reiciendis fugit impedit itaque maxime quidem.
-            Odit ducimus possimus nam, cum non facilis laudantium dolorum quod
-            harum deserunt, atque ad explicabo vero aliquid?
+            harum deserunt, atque ad explicabo vero aliquid? Lorem ipsum, dolor
+            sit amet consectetur adipisicing elit. Voluptatibus voluptate ullam
+            veniam omnis quibusdam totam alias cupiditate, magni amet autem
+            reprehenderit laudantium quaerat labore quis deserunt nulla
+            reiciendis fugit impedit itaque maxime quidem. Odit ducimus possimus
+            nam, cum non facilis laudantium dolorum quod harum deserunt, atque
+            ad explicabo vero aliquid? Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Voluptatibus voluptate ullam veniam omnis
+            quibusdam totam alias cupiditate, magni amet autem reprehenderit
+            laudantium quaerat labore quis deserunt nulla reiciendis fugit
+            impedit itaque maxime quidem. Odit ducimus possimus nam, cum non
+            facilis laudantium dolorum quod harum deserunt, atque ad explicabo
+            vero aliquid?
           </p>
         </div>
-        <div className="flex flex-col gap-4 border rounded-md p-8 w-1/4">
+        <div className="flex flex-col gap-4 border rounded-md p-8 w-full md:w-1/4 overflow-hidden">
           <h2 className="text-xl font-bold">Produtos mais vendidos</h2>
           <p>Gráfico</p>
           <p>
