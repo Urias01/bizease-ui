8e8ea44508bb98bbdbc4d6f3155bc6d148a975ca
Urias Pereira <uriasluis64@gmail.com>
2024-11-27 18:14:59 -0300

    feat: update commerce dialog

--- src/components/edit-commerce.tsx
+++ src/components/edit-commerce.tsx
@@ -10,8 +10,8 @@ import {
 import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { FormItem } from "./ui/form";
 
 interface EditCommerceProps {
   onClose: () => void;
@@ -19,7 +19,7 @@ interface EditCommerceProps {
 
 export function EditCommerce({ onClose }: EditCommerceProps) {
   return (
-    <DialogContent>
+    <DialogContent className="min-w-fit">
       <DialogHeader>
         <DialogTitle>Edite seu comércio</DialogTitle>
         <DialogDescription>
@@ -29,60 +29,63 @@ export function EditCommerce({ onClose }: EditCommerceProps) {
       </DialogHeader>
       <Separator className="w-full" />
       <form>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
-              Cnpj
-            </Label>
-            <Input id="name" className="col-span-3" />
+        <div className="grid grid-cols-12 gap-4 py-4">
+          <div className="grid-cols-6 col-span-6">
+            <FormItem>
+              <Label htmlFor="email" className="text-right">
+                Cnpj
+              </Label>
+              <Input id="name" className="col-span-3" />
+            </FormItem>
+            <FormItem>
+              <Label htmlFor="name" className="text-right">
+                Nome
+              </Label>
+              <Input id="name" className="col-span-3" />
+            </FormItem>
+            <FormItem>
+              <Label htmlFor="name" className="text-right">
+                Telefone
+              </Label>
+              <Input id="name" className="col-span-3" />
+            </FormItem>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Nome
-            </Label>
-            <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Telefone
-            </Label>
-            <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-baseline gap-4">
-            <Label htmlFor="description" className="text-right">
-              Descrição
-            </Label>
-            <Textarea id="description" className="col-span-3 min-h-[100px]" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              CEP
-            </Label>
-            <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Endereço
-            </Label>
-            <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Número
-            </Label>
-            <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Cidade
-            </Label>
-            <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Bairro
-            </Label>
-            <Input id="name" className="col-span-3" />
+          <div className="grid-cols-6 col-span-6">
+            <FormItem className="">
+              <Label htmlFor="name" className="text-right">
+                CEP
+              </Label>
+              <Input id="name" className="col-span-3" />
+            </FormItem>
+            <div className="grid grid-cols-6 gap-4">
+              <FormItem className="col-span-4">
+                <Label htmlFor="address">Endereço</Label>
+                <Input id="address" />
+              </FormItem>
+              <FormItem className="col-span-2">
+                <Label htmlFor="address_number">Número</Label>
+                <Input id="address_number" placeholder="Ex.. 52A" />
+              </FormItem>
+            </div>
+            <div className="grid grid-cols-6 gap-4">
+              <FormItem className="col-span-4">
+                <Label htmlFor="name" className="text-right">
+                  Cidade
+                </Label>
+                <Input id="name" className="col-span-3" />
+              </FormItem>
+              <FormItem className="col-span-2">
+                <Label htmlFor="uf">UF</Label>
+                <Input id="uf" placeholder="Ex.. SP" />
+              </FormItem>
+            </div>
+            <div className=""></div>
+            <div className="">
+              <Label htmlFor="name" className="text-right">
+                Bairro
+              </Label>
+              <Input id="name" className="col-span-3" />
+            </div>
           </div>
         </div>
         <DialogFooter>
