d1813b3056c72b6f7d5dd7f876a1557bc12c9ab4
Bhendon Soares <bhendon.soares12@gmail.com>
2024-11-29 00:02:15 -0300

    feat: integrates with back-end password change funcionality

---
+++ src/api/user/change-password.ts
@@ -0,0 +1,11 @@
+import { api } from "@/lib/axios";
+
+
+interface ChangePasswordRequest {
+    currentPassword: string;
+    newPassword: string;
+}
+
+export async function changePassword({ currentPassword, newPassword }: ChangePasswordRequest): Promise<void> {
+    await api.patch("/users/change-password", { currentPassword, newPassword });
+}
